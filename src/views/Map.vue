8b651137ec7327793d0d61bbe82dc763543f8a20
Fiqri Dwi <nurfiqri23@gmail.com>
2023-07-23 16:21:06 +0700

    refactor: change vue API styles to options api

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,28 +1,12 @@
 <template>
-	<div class="main-container">
-		<div
-			id="map"
-			:style="
-				isSidePanel
-					? 'flex-basis: 0px;'
-					: 'flex-basis: 100%;'
-			"
-		></div>
-		<div
-			class="sidepanel"
-			:style="isSidePanel ? 'width:30rem' : 'width:0'"
-		>
+	<div>
+		<div id="map"></div>
+		<div class="sidepanel" :style="isSidePanel ? 'display:block;' : 'display:none'">
 			<div class="sidepanel-menu">
-				<div
-					class="sidepanel-menu-item"
-					@click="activeMenu = 'json'"
-				>
+				<div class="sidepanel-menu-item" @click="activeMenu = 'json'">
 					<p>JSON</p>
 				</div>
-				<div
-					class="sidepanel-menu-item"
-					@click="activeMenu = 'style'"
-				>
+				<div class="sidepanel-menu-item" @click="activeMenu = 'style'">
 					<p>Style</p>
 				</div>
 			</div>
@@ -30,28 +14,16 @@
 				<div v-if="activeMenu == 'json'">
 					<div id="code-container"></div>
 				</div>
-				<div v-else-if="activeMenu == 'style'">
-					style
-				</div>
+				<div v-else-if="activeMenu == 'style'">style</div>
 			</div>
 		</div>
-		<div
-			class="sidepanel-toggle"
-			:style="
-				isSidePanel ? 'right:30rem;' : 'right:0;'
-			"
-			@click="toggleSidepanel"
-		>
-			<font-awesome-icon
-				:icon="
-					isSidePanel
-						? 'chevron-circle-right'
-						: 'chevron-circle-left'
-				"
-			/>
+
+		<!-- sidepanel button -->
+		<div class="sidepanel-toggle" :style="isSidePanel ? 'right:30%;' : 'right:0;'" @click="toggleSidepanel">
+			<font-awesome-icon :icon="isSidePanel ? 'chevron-circle-right' : 'chevron-circle-left'" />
 		</div>
 	</div>
 </template>
-<script src="./Map.ts" lang="ts"></script>
+<script src="./Map.ts"></script>
 
 <style src="../assets/styles/Map.scss" lang="scss"></style>
